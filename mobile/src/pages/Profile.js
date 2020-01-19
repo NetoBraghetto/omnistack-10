d4b93e76df475e63aae4754e85a9693d4b7beb98
Neto Braghetto <netow93@gmail.com>
2020-01-19 17:51:13 -0300

    Started phone

---
+++ mobile/src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, StatusBar } from "react-native";
+
+export default function Profile() {
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
+            <View />
+        </>
+    );
+}
--- src/Helpers/stringToArray.js
+++
@@ -1,3 +0,0 @@
-module.exports = function stringToArray(str, separator = ',') {
-    return str.split(separator).map(part => part.trim());
-}
